ddfb4a1b49d8c221c52adaf198888410ea4d62d4
Santiagoalba <sanalba2222@gmail.com>
2021-02-04 21:25:54 -0300

    inicio clon amazon

---
+++ commerce-clone/src/components/Product.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../css/Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean Startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+      </div>
+
+      <div className="product__rating">
+        <p>⭐</p>
+        {/* <p>⭐</p>
+        <p>⭐</p>
+        <p>⭐</p>
+        <p>⭐</p> */}
+      </div>
+
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        className="product__image"
+      ></img>
+
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
---
+++ commerce-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEhvOAkdXQvH6T2bSVL0NddHw3y6-ILXE",
+  authDomain: "commerce-clone.firebaseapp.com",
+  projectId: "commerce-clone",
+  storageBucket: "commerce-clone.appspot.com",
+  messagingSenderId: "657393143069",
+  appId: "1:657393143069:web:22da95e62b5c25ca5a65f4",
+  measurementId: "G-BE6N37D89C"
+};
