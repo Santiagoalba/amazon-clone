9ba7db50e70522ad3809e7f40236ecfee449ca2d
Santiagoalba <sanalba2222@gmail.com>
2021-02-06 00:04:08 -0300

    checkoout

---
+++ commerce-clone/src/components/Checkout.js
@@ -0,0 +1,28 @@
+import "../css/Checkout.css";
+import Subtotal from "../components/Subtotal";
+
+import React from "react";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="amazonAd"
+        ></img>
+
+        <div>
+          <h2 className="checkout__title">Your shopping Bastet</h2>
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
--- commerce-clone/src/components/Header.js
+++ commerce-clone/src/components/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import "../css/Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text"></input>
@@ -30,7 +33,9 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
         <div className="header__optionBasket">
-          <ShoppingBasketIcon />
+          <Link to="/checkout">
+            <ShoppingBasketIcon />
+          </Link>
           <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
